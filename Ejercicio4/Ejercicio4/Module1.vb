b87b5a047f9153a6deb1362090509120a397a629
Oscar Mejia <dsnp011423@ugb.edu.sv>
2023-09-23 12:56:33 -0600

    5 Ejercicios tarea Bloque 1

---
+++ Ejercicio4/Ejercicio4/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+    ' Oscar Enrique Mejía Chávez DSNP011423
+    Sub Main()
+        ' Declaracion de variables
+        Dim n As Integer
+        Dim i As Integer
+        Dim suma As Integer
+        suma = 0
+
+        ' Ingreso del número a calcular
+        Console.WriteLine("---------------------------------------")
+        Console.WriteLine("Ingrese un numero positivo mayor que 0:")
+        Console.WriteLine("---------------------------------------")
+        n = Console.ReadLine()
+
+        ' Evaluación del número 
+        If n > 0 Then
+            ' Ciclo for para n números
+            For i = 1 To n
+                suma += i
+            Next
+            Console.WriteLine("----------------------------------------------------------------")
+            Console.WriteLine("La suma de los números naturales hasta " & n & " es igual a: " & suma)
+            Console.WriteLine("----------------------------------------------------------------")
+            Console.WriteLine()
+            Console.WriteLine("Presione cualquier tecla para salir...")
+            Console.ReadKey()
+        Else
+            ' Mensaje de error
+            Console.WriteLine("---------------------------------------")
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine(" ¡¡¡El numero ingresado es invalido!!!")
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine("---------------------------------------")
+            Console.WriteLine()
+            Console.WriteLine("Presione cualquier tecla para salir...")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
---
+++ Ejercicio5/Ejercicio5/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    ' Oscar Enrique Mejía Chávez DSNP011423
+    Sub Main()
+        ' Declaración de variables
+        Dim num As Integer
+        Dim tabla As Integer
+        Dim i As Integer
+        ' Ingresar el número
+        Console.WriteLine("-------------------------------------------------------")
+        Console.WriteLine("Ingrese el número del que desea la tabla de multiplicar")
+        Console.WriteLine("-------------------------------------------------------")
+        num = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine("-----------------------------------------")
+        Console.WriteLine("La tabla de multiplicar del número " & num & " es:")
+        Console.WriteLine("-----------------------------------------")
+        Console.WriteLine()
+        Console.WriteLine("-------------")
+        For i = 1 To 10
+            tabla = i * num
+            Console.WriteLine(num & " x " & i & " = " & tabla)
+        Next
+        Console.WriteLine("-------------")
+        Console.WriteLine("")
+        Console.WriteLine("--------------------------------------")
+        Console.WriteLine("Presione cualquier tecla para salir...")
+        Console.WriteLine("--------------------------------------")
+        Console.ReadKey()
+    End Sub
+
+End Module
